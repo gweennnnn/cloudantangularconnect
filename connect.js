4d96767ee952e1f994a9ca366366a0c8032234a2
gweennnnn <me@gwendolyn.info>
2015-11-16 22:56:22 +0000

    first commit

---
+++ connect.js
@@ -0,0 +1,40 @@
+﻿var app = angular.module('myApp', []);
+const ACCOUNTNAME = 'YOUR_ACCOUNT_NAME';
+const APIKEY = 'YOUR_API_KEY';
+const APIPASSWORD = 'YOUR_API_PASSWORD';
+const DATABASE = 'DATABASE_TO_ACCESS';
+
+
+app.controller('tutorialCtrl', function($scope, $http) {
+	//Authentication header settings
+    var req = {
+		method: 'POST',
+		url: 'https://'+ACCOUNTNAME+'.cloudant.com/_session',
+		headers: {
+	   		'Content-Type': 'application/x-www-form-urlencoded'
+	 	},
+	 	data: 'name='+APIKEY+'&password='+APIPASSWORD,
+	 	withCredentials: true
+	}
+	$http(req).then(function(result){
+						//Grab data
+						req = {
+							method: 'GET',
+							url: 'https://'+ACCOUNTNAME+'.cloudant.com/'+DATABASE+'/_all_docs',
+	 						withCredentials: true
+						}
+						
+						$http(req).then(function(result){
+											$scope.tableData = result.data;
+										},
+										function(){
+											console.log("Failed at grabbing data");
+										});
+
+					}, 
+					function(){
+						console.log('Failed at authenticating');
+					}
+	);
+});
+    
